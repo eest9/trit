0373422d3b32e2bb2a3a6f2ab50c3904a5ce939d
eest9 <eestoneee@gmail.com>
2015-02-15 20:50:44 +0100

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include "ternary.h"
+
+using namespace std;
+
+int main()
+{
+    short byte=0;
+    short tryte[11];
+    do{
+    cout << "Eingabe:     ";
+    cin >> byte;
+    cout << "Dezimal:     " << byte << endl;
+    var_to_tryte(byte, tryte);
+    cout << "Ternär Bal.: ";
+    for(char i=0;i<11;i++)
+    {
+        cout << tryte[i] << " ";
+    }
+    cout << endl;
+    cout << "Ternär Bal.: ";
+    for(char i=0;i<11;i++)
+    {
+        if(tryte[i]==-1)
+        {
+            cout << "-";
+        }
+        if(tryte[i]==1)
+        {
+            cout << "+";
+        }
+        if(tryte[i]==0)
+        {
+            cout << "0";
+        }
+    }
+    cout << endl;
+    byte++;
+    }while(byte!=0);
+    return 0;
+}
